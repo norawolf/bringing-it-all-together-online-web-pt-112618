441b417528897cc4addd4d3ce620a1a14f6d0caf
norawolf <noraevogt@gmail.com>
2019-02-16 11:31:24 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,80 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs;"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+        sql = <<-SQL
+          INSERT INTO dogs (name, breed)
+          VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        self
+    end
+  end
+
+  def self.create(attributes)
+    Dog.new(attributes).tap {|dog_obj| dog_obj.save}
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?;"
+    result = DB[:conn].execute(sql, id)[0]
+    # how are we able to change the order of these args but it still works?
+    # in initialize, are keyword arguments not ordered?
+    Dog.new(id: result[0], name: result[1], breed: result[2])
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?;"
+    dog = DB[:conn].execute(sql, name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
+    Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+end
